6b40b1aa6b8a63f6659c9d99fd31a947d4fc0599
NEMESIS13cz <OfficialNemes@gmail.com>
2016-10-30 08:46:12 +0100

    Dokončení generátoru ID

--- LibrarySoftware/server/Database.cs
+++ LibrarySoftware/server/Database.cs
@@ -175,8 +175,7 @@ namespace LibrarySoftware.server
             {
                 borrowed = borrowed.Substring(0, borrowed.Length - 1);
             }
-            SQLiteCommand idCmd = new SQLiteCommand("SELECT Count(*) FROM users;", db);
-            reader.ID = idCmd.ExecuteScalar().ToString();
+            reader.ID = Authenticator.hashPassword(Convert.ToString(DateTime.UtcNow.Ticks) + reader.email);
 
             query("INSERT INTO users (name, address, birthNumber, birthDate, email, borrowedBooks, " + 
                 "reservedBooks, admin, password, id) VALUES (@name, @address, @birthNumber, @birthDate, @email, " + 
